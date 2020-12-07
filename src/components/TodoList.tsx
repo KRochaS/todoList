2490d31e3075344ee5eba45d4b7e183190b299f4
KRochaS <karinerocha.s@hotmail.com>
2020-12-07 20:36:38 -0300

    TodoList and TodoListItem

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Todo } from '../models/Todo';
+import TodoListItem from './TodoListItem';
 
 
 const TodoList = () => {
-    return(
-        <div>
-            Mys todos list
-        </div>
+    const todos: Todo[] = [
+        { id: 1, title: 'Study English', done: false },
+        { id: 2, title: 'Listen podcasts', done: true }
+    ];
+
+    return (
+        <table className="uk-table">
+            <caption>
+                Todo List
+                    </caption>
+            <thead>
+                <tr>
+                    <th> #</th>
+                    <th> Tarefa </th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+
+            {todos?.map((todo: Todo) => {
+                    return <TodoListItem key={todo.id} todo={todo} />
+                })}
+            </tbody>
+        </table>
+
     )
+
+
 
 }
 
